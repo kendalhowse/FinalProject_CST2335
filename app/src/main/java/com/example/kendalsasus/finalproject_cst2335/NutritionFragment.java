44083cae9207f54f9ffb62cea90863326fa3ca49
Chris Labelle <chris.labelle17@gmail.com>
2017-12-13 19:02:32 -0500

    Added a toast to Nutrition Fragment, as well as some comments in the code.

--- app/src/main/java/com/example/kendalsasus/finalproject_cst2335/NutritionFragment.java
+++ app/src/main/java/com/example/kendalsasus/finalproject_cst2335/NutritionFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -51,13 +52,18 @@ public class NutritionFragment extends Fragment {
         addNutritionEntryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View handlerView) {
-
+                // is the EditText for Food Item empty?
                 if(etAddNutritionItem.getText() == null || etAddNutritionItem.getText().equals("")) {
-
-                    // MAKE A TOAST WARNING HERE... THAT FIELD CAN'T BE EMPTY
+                    // if so, alert the user
+                    Toast.makeText(
+                            handlerView.getContext(),
+                            "You cannot leave Food Item field empty.",
+                            Toast.LENGTH_LONG
+                    ).show();
                 }
                 else {
-
+                    // otherwise, store info from System.currentTimeMillis and Food Item
+                    // EditText in a bundle
                     Bundle formValues = getNewEntryInfo(layoutView);
                     formValues.putString("Item", etAddNutritionItem.getText().toString());
                     formValues.putLong("Timestamp", System.currentTimeMillis());
@@ -66,10 +72,13 @@ public class NutritionFragment extends Fragment {
                     completedForm.putExtras(formValues);
                     getActivity().setResult(10, completedForm);
 
+                    // if the device is a tablet
                     if(isTablet) {
+                        // remove the fragment
                         getActivity().getFragmentManager().beginTransaction().remove(NutritionFragment.this).commit();
                     }
                     else {
+                        // otherwise finish the activity
                         getActivity().finish();
                     }
                 }
@@ -79,10 +88,12 @@ public class NutritionFragment extends Fragment {
 
     private Bundle getNewEntryInfo(View view) {
         Bundle info = new Bundle();
+        // get reference to EditTexts in add_nutrition_entry layout
         EditText etAddNutritionCalories = view.findViewById(R.id.add_nutrition_calories_edit_text);
         EditText etAddNutritionFat = view.findViewById(R.id.add_nutrition_fat_edit_text);
         EditText etAddNutritionCarbs = view.findViewById(R.id.add_nutrition_carbs_edit_text);
 
+        // initialize them to 0, if there's an error with input, or user doesn't enter anything, they stay as 0
         double calories = 0;
         double fat = 0;
         double carbs = 0;
@@ -90,6 +101,7 @@ public class NutritionFragment extends Fragment {
         try{fat = Double.parseDouble(etAddNutritionFat.getText().toString());} catch(Exception e) {}
         try{carbs = Double.parseDouble(etAddNutritionCarbs.getText().toString());} catch(Exception e) {}
 
+        // store the values of the EditTexts in a bundle
         info.putDouble("Calories", calories);
         info.putDouble("Fat", fat);
         info.putDouble("Carbohydrates", carbs);
