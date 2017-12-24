9cbd6987db4d84c2d9c7d2cffce8f17b40bbbb29
MRajjy <raja0060@algonquinlive.com>
2017-12-23 23:25:20 -0500

    implemented help dialog for Activity Tracker

--- app/src/main/java/com/example/kendalsasus/finalproject_cst2335/ActivityTracker.java
+++ app/src/main/java/com/example/kendalsasus/finalproject_cst2335/ActivityTracker.java
@@ -1,10 +1,16 @@
 package com.example.kendalsasus.finalproject_cst2335;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityTracker extends MainActivity {
 
@@ -38,4 +44,52 @@ public class ActivityTracker extends MainActivity {
     // monthly statistics
 
     // *progress bar, button, edittext, toast, snackbar, dialog, help menu item, support one other language (French - will make another strings.xml at the end)
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+
+        super.onOptionsItemSelected(menuItem);
+
+        if (menuItem.getItemId() == R.id.mi_help){
+
+            Log.d("Toolbar", "Option 3 selected");
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(ActivityTracker.this);
+            // Get the layout inflater
+            LayoutInflater inflater = this.getLayoutInflater();
+            final LinearLayout rootTag = (LinearLayout)inflater.inflate(R.layout.dialog_box, null);
+            final TextView author = (TextView)rootTag.findViewById(R.id.author);
+            final TextView version = (TextView)rootTag.findViewById(R.id.versionNum);
+            final TextView instructions = (TextView)rootTag.findViewById(R.id.instructions);
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(inflater.inflate(R.layout.dialog_box, null))
+
+                    //set view as root tag
+                    .setView(rootTag)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //do nothing - except exit dialog
+                            Toast t = Toast.makeText(ActivityTracker.this, "Exited Help", Toast.LENGTH_LONG);
+                            t.show();
+                        }
+                    });
+
+                    // Add content for activity help
+                    author.setText("Author: Melissa Rajala");
+                    version.setText("Version: 1.0");
+                    instructions.setText("1.");
+
+
+
+            builder.create();
+            builder.show();
+
+        }
+
+        return true;
+
+    }
+
 }
