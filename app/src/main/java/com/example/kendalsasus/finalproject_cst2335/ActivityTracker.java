94f720321dd1d9d07fdff5a44c57bac6704ea80d
MRajjy <raja0060@algonquinlive.com>
2017-12-23 23:53:40 -0500

    save point for ActivityTracker

--- app/src/main/java/com/example/kendalsasus/finalproject_cst2335/ActivityTracker.java
+++ app/src/main/java/com/example/kendalsasus/finalproject_cst2335/ActivityTracker.java
@@ -1,16 +1,35 @@
 package com.example.kendalsasus.finalproject_cst2335;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.ArrayList;
+
+//enter activities:
+// Activity label - pull down with 5 options (Running, Walking, Biking, Swimming, Skating)
+// Time label - enter amount of time using textview (x2) - have selection for minutes and hours
+// Comment label - textview to enter comment
+// current time attribute - enter into dB too
+
+//have a listview to show activities previously entered (my activity history) - use fragment - selecting item by date shows details in fragment
+// should be able to delete or edit past entries and commit back to the dB
+// monthly statistics
+
+// *progress bar, button, edittext, toast, snackbar, dialog, help menu item, support one other language (French - will make another strings.xml at the end)
+
 
 public class ActivityTracker extends MainActivity {
 
@@ -19,6 +38,33 @@ public class ActivityTracker extends MainActivity {
     ImageButton addBtn;
     ImageButton deleteBtn;
     ImageButton editBtn;
+    ArrayList<String> msgs = new ArrayList<>(); //tmp will change this
+    DatabaseHelper dh;
+    Cursor results;
+
+    //chat adaptor
+    private class ChatAdapter extends ArrayAdapter<String> {
+        public ChatAdapter (Context ctx){
+            super(ctx, 0);
+        }
+
+        //returns number of lines in chat listview
+        public int getCount(){
+            return msgs.size();
+        }
+
+        public String getItem(int position){
+            return msgs.get(position);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = ActivityTracker.this.getLayoutInflater();
+            View result = null;
+
+            return result;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,19 +79,7 @@ public class ActivityTracker extends MainActivity {
 
     }
 
-    //enter activities:
-    // Activity label - pull down with 5 options (Running, Walking, Biking, Swimming, Skating)
-    // Time label - enter amount of time using textview (x2) - have selection for minutes and hours
-    // Comment label - textview to enter comment
-    // current time attribute - enter into dB too
-
-    //have a listview to show activities previously entered (my activity history) - use fragment - selecting item by date shows details in fragment
-    // should be able to delete or edit past entries and commit back to the dB
-    // monthly statistics
-
-    // *progress bar, button, edittext, toast, snackbar, dialog, help menu item, support one other language (French - will make another strings.xml at the end)
-
-
+    //clicking the help menu item
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem) {
 
@@ -53,7 +87,7 @@ public class ActivityTracker extends MainActivity {
 
         if (menuItem.getItemId() == R.id.mi_help){
 
-            Log.d("Toolbar", "Option 3 selected");
+            Log.i("ActivityTracker", "Help menu option selected");
 
             AlertDialog.Builder builder = new AlertDialog.Builder(ActivityTracker.this);
             // Get the layout inflater
@@ -80,16 +114,12 @@ public class ActivityTracker extends MainActivity {
                     author.setText("Author: Melissa Rajala");
                     version.setText("Version: 1.0");
                     instructions.setText("1.");
-
-
+                    //TO DO: Finish instructions
 
             builder.create();
             builder.show();
-
         }
-
         return true;
-
     }
 
 }
