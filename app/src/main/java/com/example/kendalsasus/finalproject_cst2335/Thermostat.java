77a9fc2a22953913514ac507361737833db03451
Steven Adema <stevenadema@gmail.com>
2017-12-30 22:16:59 -0500

    thermostat layout updated

--- app/src/main/java/com/example/kendalsasus/finalproject_cst2335/Thermostat.java
+++ app/src/main/java/com/example/kendalsasus/finalproject_cst2335/Thermostat.java
@@ -1,13 +1,18 @@
 package com.example.kendalsasus.finalproject_cst2335;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class Thermostat extends MainActivity {
+/**
+ * Created by Steven Adema on 30-Dec-2017.
+ */
+
+public class Thermostat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thermostat);
     }
+
 }
---
+++ app/src/main/java/com/example/kendalsasus/finalproject_cst2335/ThermostatAddEntry.java
@@ -0,0 +1,19 @@
+package com.example.kendalsasus.finalproject_cst2335;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Steven on 12/30/2017.
+ */
+
+public class ThermostatAddEntry extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_thermostat_add_entry);
+        }
+
+}
